4f14f7600742f420a565407e76028502171e5bec
jvilders <60934195+jvilders@users.noreply.github.com>
2024-09-01 15:51:38 +0200

    fix: stage fixed files from pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,18 +3,27 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import fs from "fs-extra";
+import path from "path";
+
+// With typed linting, eslint depends on the typescript compiler. I use an eslint specific config
+// because I want to check more files than only those I include in the bundled package. Eslint will
+// fail to lint any files not included by that config. Instead of duplicating the include patterns
+// from that config, I read them in and use them.
+function getTsConfigIncludePatterns() {
+  const filepath = path.join(import.meta.dirname, "tsconfig.eslint.json");
+  const config = JSON.parse(fs.readFileSync(filepath, "utf-8"));
+  return config.include;
+}
+const tsconfigIncludedGlobPatterns = getTsConfigIncludePatterns();
 
 export default tseslint.config(
   // Unless 'ignores' is the only key in the object, the given patterns only apply to that specific configuration.
   // These ignore patterns are intended to be global, so it's in a separate object, even though it looks goofy.
   {
-    ignores: [
-      "eslint.config.mjs",
-      "commitlint.config.ts",
-      "vitest.config.mts",
-      "build/",
-      "coverage/",
-    ],
+    // You would expect that specifying 'files' would be enough and that everything else would be ignored,
+    // you would be wrong (unfortunately). Therefore instead of selecting, I 'unignore' what I want to select.
+    ignores: ["*", ...tsconfigIncludedGlobPatterns.map((p) => `!${p}`)],
   },
   {
     languageOptions: {
